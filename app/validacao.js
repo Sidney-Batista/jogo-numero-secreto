b88440ad8bb0ff3b86a180b0bf7c8618c2d2f04b
Sidney-Batista <80222651+Sidney-Batista@users.noreply.github.com>
2022-07-24 13:30:35 -0300

    publishing the game

---
+++ app/validacao.js
@@ -0,0 +1,40 @@
+function verificaSeOChutePossuiUmValorValido(){
+    const numero = +chute
+
+    if(chuteForInvalido(numero)){
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
+        return
+    }
+    
+    if(numeroForMaiorOuMenorQueValorPermitido(numero)){
+        elementoChute.innerHTML += `<div>valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>`
+        return
+    }
+
+    if(numero === numeroSecret){
+        document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era ${numeroSecret}</h3>        
+        <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+        `
+    }else if(numero > numeroSecret){
+        elementoChute.innerHTML +=`<div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>`
+    } else{
+        elementoChute.innerHTML +=`<div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`
+    }
+
+}
+
+function chuteForInvalido(numero){
+    return Number.isNaN(numero)
+}
+
+function numeroForMaiorOuMenorQueValorPermitido(numero){
+    return numero > maiorValor || numero <menorValor
+}
+
+document.body.addEventListener('click', e =>{
+    if(e.target.id == 'jogar-novamente'){
+        window.location.reload()
+    }
+})
